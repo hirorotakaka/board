10fdca32e17180cb1a0cfaba4be7d619af20e5c8
hirotaka <thekidoruaaa2020@gmail.com>
2023-01-26 10:42:00 +0900

    board ver2 #2

--- src/main/java/com/example/board/repository/Post.java
+++ src/main/java/com/example/board/repository/Post.java
@@ -1,5 +1,9 @@
 package com.example.board.repository;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -17,30 +21,37 @@ import lombok.Data;
 @Data
 public class Post {
 
-    /** ID */
-    @Id
-    @Column
-    private String id = null;
+	/** ID */
+	@Id
+	@Column
+	/*private String id = null;*/
+	@NotNull
+	private String id = null;
+	/** 投稿者 */
+	@Column(length = 20, nullable = false)
+	/*private String author = null;*/
+	@NotEmpty
+	@Size(min = 1, max = 20)
+	private String author = null;
+	/** タイトル */
+	@Column(length = 20, nullable = false)
+	/*private String title = null;*/
+	@NotEmpty
+	@Size(min = 1, max = 20)
+	private String title = null;
+	/** 内容 */
+	@Column(length = 1000, nullable = false)
+	/*private String body = null;*/
+	@NotEmpty
+	@Size(min = 1, max = 1000)
+	private String body = null;
+	/** 登録日時 */
+	private Date createdDate = null;
 
-    /** 投稿者 */
-    @Column(length = 20, nullable = false)
-    private String author = null;
+	/** 更新日時 */
+	private Date updatedDate = null;
 
-    /** タイトル */
-    @Column(length = 20, nullable = false)
-    private String title = null;
-
-    /** 内容 */
-    @Column(length = 1000, nullable = false)
-    private String body = null;
-
-    /** 登録日時 */
-    private Date createdDate = null;
-
-    /** 更新日時 */
-    private Date updatedDate = null;
-
-    /** 削除済 */
-    private boolean deleted = false;
+	/** 削除済 */
+	private boolean deleted = false;
 
 }
